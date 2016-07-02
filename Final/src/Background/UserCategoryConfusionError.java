eda2d6bca5e23fb264202631b69b927e41967833
yurico <yurikoif@gmail.com>
2016-07-02 16:22:34 -0400

    add

---
+++ Final/src/Background/UserCategoryConfusionError.java
@@ -0,0 +1,15 @@
+package Background;
+
+public class UserCategoryConfusionError extends Error {
+
+    private static final long serialVersionUID = 1L;
+
+    public UserCategoryConfusionError() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public UserCategoryConfusionError(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+}
