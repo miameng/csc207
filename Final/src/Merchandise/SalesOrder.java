0696d85dfb53d8f0a5c228896f011c8c8029be3d
yurico <yurikoif@gmail.com>
2016-07-02 15:12:00 -0400

    Modified!

--- Final/src/Merchandise/SalesOrder.java
+++
@@ -1,9 +0,0 @@
-package Merchandise;
-
-public class SalesOrder extends Order {
-	
-	public SalesOrder(Merchandise m, int quantity) {
-		super(m, quantity);
-		// TODO Auto-generated constructor stub
-	}
-}
