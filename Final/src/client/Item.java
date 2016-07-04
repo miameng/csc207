8a8b893735d0f8e563a3d964fa04402b1974d6c4
yurico <yurikoif@gmail.com>
2016-07-04 14:21:18 -0400

    Update

---
+++ Final/src/client/Item.java
@@ -0,0 +1,11 @@
+package client;
+
+import java.awt.Image;
+import java.util.List;
+
+public class Item {
+    Image image;
+    String description;
+    int ID, quantity;
+    List<String> tags;
+}
